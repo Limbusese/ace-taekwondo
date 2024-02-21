f42f65a59f8b4a54f86d894bffe4c53c7feef81a
Limbusese <135493509+Limbusese@users.noreply.github.com>
2024-02-21 10:54:33 +0545

    Updated index.js

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -39,7 +39,3 @@ navAnchors.addEventListener("click", ()=> {
 });
 
 
-const width = window.screen.width;
-const height = window.screen.height;
-
-alert (`The screen resolution is ${width} * ${height}`);
